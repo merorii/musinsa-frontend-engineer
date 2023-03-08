23f24879bffa57cf016cba35dec9d1c470c9d69c
merorii <hyeseon0319@gmail.com>
2023-03-08 15:28:29 +0900

    feat: 데이터 filtering hook 추가

---
+++ src/hooks/useFilterData.ts
@@ -0,0 +1,27 @@
+import { useRecoilValue } from "recoil";
+import { filterState } from "store/filterState";
+
+export const useFilterData = (result: any) => {
+  const filter = useRecoilValue(filterState);
+
+  const data = () => {
+    if (!result) return;
+    let updated = result;
+
+    filter.forEach((activeFilter) => {
+      if (activeFilter === "isFemale") {
+        updated = updated.filter((item: any) => item.gender === "Female");
+      }
+      if (activeFilter === "isLive") {
+        updated = updated.filter((item: any) => item.died === "");
+      }
+      if (activeFilter === "noTvSeries") {
+        updated = updated.filter((item: any) => item.tvSeries.length <= 1);
+      }
+    });
+
+    return updated;
+  };
+
+  return data();
+};
