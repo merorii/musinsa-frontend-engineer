ece7d59408da11b8786396ba83dbd48bc532eec9
merorii <hyeseon0319@gmail.com>
2023-03-08 13:05:37 +0900

    init: 프로젝트 기본 세팅

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle` 
+    
+  body{
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+        sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+  input:focus {
+    outline: none;
+  }
+
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
